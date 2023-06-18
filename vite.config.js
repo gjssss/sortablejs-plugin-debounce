17d793547189a423ec917ad7989e5249a6b94c83
gjssss <gaojinsong2010@gmail.com>
2023-06-18 20:19:10 +0800

    fix test example

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.js"),
-      name: "sortablejs-plugin-debounce",
+      name: "SortablejsDebounce",
+      fileName: (format) => `sortablejs-plugin-debounce.${format}.js`,
     },
   },
 });
